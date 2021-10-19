95ee85f245e83c56a7b95848a98d2d59350bf29d
CodeAkio <siqueira_2004@hotmail.com>
2021-10-19 19:50:01 -0300

    Create home layout

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,24 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("assets/images/yt_logo_rgb_dark.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        actions: [
+          const Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.star)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
